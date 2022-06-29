67c818fc80b8a66dffe5698f19b34ff223b4cb3f
Lukas Ettenauer <lukas.ettenauer@outlook.com>
2022-06-29 22:47:18 +0200

    feat: add Start to Listen to Yourself pattern

--- Trinkhalle.Api/BeverageManagement/CreateBeverage.cs
+++ Trinkhalle.Api/BeverageManagement/CreateBeverage.cs
@@ -1,40 +1,78 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.ServiceBus;
 using Trinkhalle.Api.Domain;
 using Trinkhalle.Api.Infrastructure;
 
 namespace Trinkhalle.Api.BeverageManagement;
 
-public record CreateBeverageCommand : IRequest<Guid>
-{
-    public string Name { get; init; } = null!;
-    public decimal Price { get; init; }
-    public string ImageUrl { get; init; } = null!;
-    public bool Available { get; init; }
-}
-
 public class CreateBeverage
 {
+    public record BeverageCreatedEvent
+    {
+        public Guid Id { get; init; }
+        public string Name { get; init; } = null!;
+        public decimal Price { get; init; }
+        public string ImageUrl { get; init; } = null!;
+        public bool Available { get; init; }
+    }
+
+    public record CreateBeverageCommand : IRequest<Guid>
+    {
+        public Guid Id { get; init; }
+        public string Name { get; init; } = null!;
+        public decimal Price { get; init; }
+        public string ImageUrl { get; init; } = null!;
+        public bool Available { get; init; }
+    }
+
     private readonly IMediator _mediator;
+    private const string BeverageCreatedTopic = "BeverageCreated";
+    private const string BeverageCreatedEventSubscriber = "BeverageManagement_BeverageCreatedEventConsumer";
 
     public CreateBeverage(IMediator mediator)
     {
         _mediator = mediator;
     }
 
-    [FunctionName("CreateBeverage")]
+    [FunctionName("BeverageManagement_CreateBeverage")]
     public async Task<IActionResult> RunAsync(
-        [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]
-        CreateBeverageCommand createBeverageCommand)
+        [HttpTrigger(AuthorizationLevel.Function, "post", Route = "BeverageManagement/CreateBeverage")]
+        CreateBeverageCommand createBeverageCommand, CancellationToken cancellationToken,
+        [ServiceBus(queueOrTopicName: BeverageCreatedTopic, EntityType = ServiceBusEntityType.Topic,
+            Connection = "AzureServiceBus")]
+        ServiceBusSender sender)
     {
-        var beverageId = await _mediator.Send(createBeverageCommand);
+        var beverageCreatedEvent = new BeverageCreatedEvent()
+        {
+            Id = Guid.NewGuid(), Available = createBeverageCommand.Available, Name = createBeverageCommand.Name,
+            Price = createBeverageCommand.Price, ImageUrl = createBeverageCommand.ImageUrl
+        };
 
-        return new OkObjectResult(beverageId);
+        await sender.SendMessageAsync(new ServiceBusMessage(new BinaryData(beverageCreatedEvent)), cancellationToken);
+
+        return new OkObjectResult(beverageCreatedEvent.Id);
+    }
+
+    [FunctionName(BeverageCreatedEventSubscriber)]
+    public async Task Run(
+        [ServiceBusTrigger(topicName: BeverageCreatedTopic, subscriptionName: BeverageCreatedEventSubscriber,
+            Connection = "AzureServiceBus")]
+        BeverageCreatedEvent beverageCreatedEvent, CancellationToken cancellationToken)
+    {
+        await _mediator.Send(
+            new CreateBeverageCommand()
+            {
+                Id = beverageCreatedEvent.Id, Available = beverageCreatedEvent.Available,
+                Name = beverageCreatedEvent.Name, ImageUrl = beverageCreatedEvent.ImageUrl,
+                Price = beverageCreatedEvent.Price
+            }, cancellationToken);
     }
 
     public class CreateBeverageCommandHandler : IRequestHandler<CreateBeverageCommand, Guid>
@@ -48,8 +86,7 @@ public class CreateBeverage
 
         public async Task<Guid> Handle(CreateBeverageCommand request, CancellationToken cancellationToken)
         {
-            var beverage = new Beverage(Guid.NewGuid(), request.Price, request.Name, request.ImageUrl,
-                request.Available);
+            var beverage = new Beverage(request.Id, request.Price, request.Name, request.ImageUrl, request.Available);
 
             _dbContext.Beverages.Add(beverage);
             await _dbContext.SaveChangesAsync(cancellationToken);
